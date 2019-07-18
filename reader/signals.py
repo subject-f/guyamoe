b8615bc34652f17ab99b804e6fe3ffc02d485afc
appu1232 <aditya.vad@gmail.com>
2019-07-18 10:03:14 -0400

    fixes for admin page and series page showing wrong time of upload

--- reader/signals.py
+++ reader/signals.py
@@ -1,10 +1,19 @@
-from reader.models import Chapter, Volume
+from reader.models import HitCount, Series, Chapter, Volume
 from django.db.models.signals import post_delete, post_save
+from django.contrib.contenttypes.models import ContentType
 from django.dispatch import receiver
 from django.conf import settings
 from api.views import clear_pages_cache
 import shutil
 import os
+
+
+@receiver(post_delete, sender=Series)
+def delete_chapter_folder(sender, instance, **kwargs):
+    series = ContentType.objects.get(app_label='reader', model='series')
+    hit_count_obj = HitCount.objects.filter(content_type=series, object_id=instance.id).first()
+    if hit_count_obj:
+        hit_count_obj.delete()
 
 @receiver(post_delete, sender=Chapter)
 def delete_chapter_folder(sender, instance, **kwargs):
@@ -19,6 +28,10 @@ def delete_chapter_folder(sender, instance, **kwargs):
             shutil.rmtree(os.path.join(folder_path, f"{str(instance.group.id)}_shrunk_blur"))
         if os.path.exists(folder_path) and not os.listdir(folder_path):
             shutil.rmtree(folder_path)
+        chapter = ContentType.objects.get(app_label='reader', model='chapter')
+        hit_count_obj = HitCount.objects.filter(content_type=chapter, object_id=instance.id).first()
+        if hit_count_obj:
+            hit_count_obj.delete()
 
 @receiver(post_delete, sender=Volume)
 def delete_volume_folder(sender, instance, **kwargs):
