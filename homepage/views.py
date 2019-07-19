dd64bd70c02a0e2d8bbf698d38f75a2b886d6703
appu1232 <aditya.vad@gmail.com>
2019-07-19 01:07:35 -0400

    home page quote changes

--- homepage/views.py
+++ homepage/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.admin.views.decorators import staff_member_required
 from django.views.decorators.cache import cache_page
 from django.core.cache import cache
+from random import randint
 from reader.models import Series, Volume, Chapter
 
 
@@ -11,7 +12,7 @@ def admin_home(request):
     return render(request, 'homepage/admin_home.html')
 
 
-@cache_page(3600 * 48)
+@cache_page(3600 * 8)
 def home(request):
     home_screen_series = {"Kaguya-Wants-To-Be-Confessed-To": "", "We-Want-To-Talk-About-Kaguya": "", "Kaguya-Wants-To-Be-Confessed-To-Official-Doujin": ""}
     for series in home_screen_series:
@@ -21,6 +22,7 @@ def home(request):
                 home_screen_series[series] = f"/media/{vol.volume_cover}"
                 break
     return render(request, 'homepage/home.html', {
+            "quote": str(randint(1, 2)),
             "abs_url": request.build_absolute_uri(),
             "main_cover": home_screen_series["Kaguya-Wants-To-Be-Confessed-To"],
             "4koma_cover": home_screen_series["We-Want-To-Talk-About-Kaguya"],
