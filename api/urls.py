43996d197b4fa60ba538dad62fed512559a86c0d
appu1232 <aditya.vad@gmail.com>
2019-07-20 22:29:30 -0400

    remove latest cover api

--- api/urls.py
+++ api/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     re_path(r'^get_all_groups/', views.get_all_groups, name='api-all-groups'),
     re_path(r'^upload_new_chapter/(?P<series_slug>[\w-]+)/', views.upload_new_chapter, name='api-chapter-upload'),
     re_path(r'^get_volume_covers/(?P<series_slug>[\w-]+)/', views.get_volume_covers, name='api-get-volume-covers'),
-    re_path(r'^get_latest_cover/(?P<series_slug>[\w-]+)/', views.get_latest_cover, name='api-get-latest-cover'),
     re_path(r'^search_index/(?P<series_slug>[\w-]+)', views.search_index, name='api-search-index'),
     re_path(r'clear_cache/', views.clear_cache, name='api-clear-cache'),
 ]
