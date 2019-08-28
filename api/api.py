977f36ce939599dd720f5dee236d8edc2762dee2
appu1232 <aditya.vad@gmail.com>
2019-08-27 23:05:26 -0400

    fix multiple group chapter download bug in api call

--- api/api.py
+++ api/api.py
@@ -114,7 +114,7 @@ def zip_series(series_slug):
     return zip_file, zip_filename
 
 def zip_chapter(series_slug, chapter):
-    ch_obj = Chapter.objects.get(series__slug=series_slug, chapter_number=chapter)
+    ch_obj = Chapter.objects.filter(series__slug=series_slug, chapter_number=chapter).first()
     chapter_dir = os.path.join(settings.MEDIA_ROOT, "manga", series_slug, "chapters", ch_obj.folder)
     groups = os.listdir(chapter_dir)
     chapter_group = None
