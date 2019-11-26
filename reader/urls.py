4afd6538e0df2e806608e9b7ae37ce359f69b78b
appu1232 <aditya.vad@gmail.com>
2019-11-25 21:51:17 -0500

    fix bug caused by trailing slash in reader url

--- reader/urls.py
+++ reader/urls.py
@@ -4,9 +4,9 @@ from reader import views
 urlpatterns = [
     re_path(r'^series/(?P<series_slug>[\w-]+)/$', views.series_info, name='reader-series'),
     re_path(r'^series/(?P<series_slug>[\w-]+)/admin$', views.series_info_admin, name='reader-series-admin'),
-    re_path(r'^series/(?P<series_slug>[\w-]+)/(?P<chapter>[\d-]{1,9})/(?P<page>[\d]{1,9})/$', views.reader, name='reader-chapter'),
+    re_path(r'^series/(?P<series_slug>[\w-]+)/(?P<chapter>[\d-]{1,9})/(?P<page>[\d]{1,9})$', views.reader, name='reader-chapter'),
     # re_path(r'^series/(?P<series_slug>[\w-]+)/(?P<chapter>[\d-]{1,9})/comments', views.chapter_comments, name='reader-chapter-comments'),
     re_path(r'^md_proxy/(?P<md_series_id>[\d]{1,9})/$', views.md_proxy, name='reader-md-proxy'),
-    re_path(r'^md_proxy/(?P<md_series_id>[\d]{1,9})/(?P<chapter>[\d-]{1,9})/(?P<page>[\d]{1,9})/$', views.md_chapter, name='reader-md-chapter'),
+    re_path(r'^md_proxy/(?P<md_series_id>[\d]{1,9})/(?P<chapter>[\d-]{1,9})/(?P<page>[\d]{1,9})$', views.md_chapter, name='reader-md-chapter'),
     re_path(r'^update_view_count/', views.hit_count, name='reader-view-count'),
 ]
