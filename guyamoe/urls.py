f2c04e8e0b5185879fce58924fc9fd19ad45bff5
funkyhippo <funkyhippo@f-ck.me>
2020-06-25 19:31:45 -0700

    Put read/ lower in the urlpattern.
    This is because reverse() follows the name that's last in the project's
    urlpatterns list.
    
    https://docs.djangoproject.com/en/3.0/topics/http/urls/#:~:text=depends%20on%20whichever%20pattern%20is%20last

--- guyamoe/urls.py
+++ guyamoe/urls.py
@@ -40,8 +40,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("homepage.urls")),
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}),
-    path("read/", include("reader.urls")),
     path("reader/", include("reader.urls")),
+    path("read/", include("reader.urls")),
     path("api/", include("api.urls")),
     path("pages/", include("misc.urls")),
     path(
