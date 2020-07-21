2aef8deba9cefd1fe8373d47987ef770eb5f209c
Einlion <65997318+Einlion@users.noreply.github.com>
2020-07-21 08:42:25 +0530

    Now I can sleep better (#56)
    * Now better
    
    * dealt all border cases
    
    * bruhhh console.log!

--- reader/static/js/init.js
+++ reader/static/js/init.js
@@ -2733,7 +2733,8 @@ function UI_Loda_Jump(o) {
 
 	this.chapPrev = "", this.pagePrev = "", this.cursorPrev = "";
 
-	this.prejump = (e, el) => {
+	this.prejump = (el) => {
+
 		if(isNaN(this._.input_chap.value)) {
 			this._.input_chap.value = this.chapPrev;
 			this._.input_chap.setSelectionRange(this.cursorPrev,this.cursorPrev); 
@@ -2744,7 +2745,7 @@ function UI_Loda_Jump(o) {
 			this._.input_page.setSelectionRange(this.cursorPrev,this.cursorPrev); 
 		}
 
-		if(parseInt(el.value) > parseInt(Reader.SCP.lastChapter) && el.selectionStart === el.value.length && !el.value.substring(el.selectionStart, el.selectionEnd) && this._.input_chap === document.activeElement) {
+		if(parseInt(el.value) > parseInt(Reader.SCP.lastChapter) && this.chapPrev.length <= Reader.SCP.lastChapter.length && el.selectionStart === el.value.length && !el.value.substring(el.selectionStart, el.selectionEnd) && this._.input_chap === document.activeElement && el.value.length > this.chapPrev.length && !el.value.endsWith(".")) {
 			this._.input_page.value = el.value.charAt(el.value.length-1);
 			el.value = el.value.substring(0, el.value.length-1);
 			this._.input_page.focus();
@@ -2757,7 +2758,7 @@ function UI_Loda_Jump(o) {
 	this._.btn.onclick = this.jump;
 	[this._.input_chap, this._.input_page].forEach(el => { 
 		el.oninput = e => {
-		 this.prejump(e, el); 
+		 this.prejump(el); 
 		}
 	});
 
