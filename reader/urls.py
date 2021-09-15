2febc646e1bf5de5ebbcf6f8f02ca7952ce2762d
appu1232 <aditya.vad@gmail.com>
2021-09-15 18:13:50 -0400

    fix canonical url resolution priority

--- reader/urls.py
+++ reader/urls.py
@@ -12,11 +12,6 @@ urlpatterns = [
         r"^manga/(?P<series_slug>[\w-]+)/$", views.series_info, name="reader-manga"
     ),
     re_path(
-        r"^manga/(?P<url_str>[\w-]+)/(?P<series_slug>[\w-]+)/$",
-        views.series_info_canonical,
-        name="reader-manga-canonical",
-    ),
-    re_path(
         r"^series/(?P<series_slug>[\w-]+)/$", views.series_info, name="reader-series"
     ),
     re_path(
@@ -48,6 +43,11 @@ urlpatterns = [
         r"^series/(?P<series_slug>[\w-]+)/(?P<chapter>[\d-]{1,9})/(?P<page>[\d]{1,9})/$",
         views.reader,
         name="reader-series-chapter",
+    ),
+    re_path(
+        r"^manga/(?P<url_str>[\w-]+)/(?P<series_slug>[\w-]+)/$",
+        views.series_info_canonical,
+        name="reader-manga-canonical",
     ),
     re_path(r"^update_view_count/", views.hit_count, name="reader-view-count"),
     re_path(
